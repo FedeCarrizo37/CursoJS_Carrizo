7544e2eb2b8311e2635353d0733a6cd1bafd925e
Federico Carrizo <fedecarrizo37@live.com.ar>
2021-10-29 11:14:49 -0300

    Clase Cuenta

--- PracticaJavaScript_2/PracticaObtenida/ejercicio13_3_Cuenta/Cuenta.js
+++ PracticaJavaScript_2/PracticaObtenida/ejercicio13_3_Cuenta/Cuenta.js
@@ -1,9 +1,28 @@
 class Cuenta{
-  constructor( numero, saldo) {
+  constructor( numero, saldo ) {
     this.numero = numero;
     this.saldo = saldo;
   }
-  metodo1() {
-    return this.propiedad1;
+  acreditar(monto) {
+    this.saldo += monto;
+  }
+  debitar(monto) {
+    this.saldo -= monto;
+  }
+  toString(){
+    return "<br>Cuenta #" + this.numero + "<br>Saldo= $" + this.saldo;
+  }
+}
+class CajaAhorro extends Cuenta {
+  constructor( numero, saldo, interes ) {
+    super( numero, saldo );
+    this.interes = interes;
+  }
+  debitar(monto){
+    if(monto <= this.saldo)
+      super.debitar(monto);
+  }
+  toString(){
+    return super.toString() + "<br>Interes= " + this.interes + "%<hr>";
   }
 }

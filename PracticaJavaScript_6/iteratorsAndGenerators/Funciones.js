e5daae60c70ab74e405d227c3b58a0870838950e
Federico Carrizo <fedecarrizo37@live.com.ar>
2021-11-10 10:04:32 -0300

    ok fibonacci

--- PracticaJavaScript_6/iteratorsAndGenerators/Funciones.js
+++ PracticaJavaScript_6/iteratorsAndGenerators/Funciones.js
@@ -9,10 +9,26 @@ function createIterator( array ){
   };
 }
 
-function *gen() {
+function *gene() {
   let i = 1;
   while (true){
     yield i;
     i = i * 2;
+  }
+}
+
+function* fibonacci () {
+  let n2 = 0;
+  let n1 = 1;
+
+  while ( true ) {
+    let input = yield n2;
+    if ( input ) {
+      n1 = 1;
+      n2 = 0;
+    } else {
+      let next = n1 + n2;
+      [ n1, n2 ] = [ next, n1 ];
+    }
   }
 }
